f95581c7796ed1b7eeebaa52f3ab2b7b57a0f4d4
jon-atto-bennett <jon.bennett@outlook.co.nz>
2017-01-29 15:20:26 +1300

    stretch minesweeper challenges

--- minesweeper.js
+++ minesweeper.js
@@ -3,6 +3,7 @@ document.addEventListener('click', checkForWin)
 document.addEventListener('contextmenu', checkForWin)
 
 
+
 // Define your `board` object here!
 // var board = {
 //
@@ -15,13 +16,20 @@ document.addEventListener('contextmenu', checkForWin)
 //         {row: 2, col: 0, isMine: true, hidden: true},
 //         {row: 2, col: 1, isMine: false, hidden: true},
 //         {row: 2, col: 2, isMine: true, hidden: true}]
-// }
-
-// function createBoard(row, col, isMine, isMarked, hidden) {
-//   this.cells = [{row, col, isMine, isMarked, hidden}, {row, col, isMine, isMarked, hidden}]
-// }
 //
-// var board = new createBoard(0,0,true,false,true);
+
+//Resets board - a fudge!!!
+function newGame() {
+    location.reload();
+}
+
+//plays sounds
+function playSound(path) {
+
+  var audioElement = document.createElement('audio');
+  audioElement.setAttribute('src', path);
+  audioElement.play();
+}
 
 var board = {
   cells: []
@@ -31,13 +39,12 @@ createBoard(3);
 
 function createBoard(squares){
 
-  console.log(squares);
   var countCol = 0;
   var countRow = 0;
 
   for(var i = 0; countRow<squares; i++ ) {
     for(var i=0; countCol<squares; i++){
-      board.cells.push({row: countRow, col: countCol, isMine: Math.random() >= 0.5, isMarked: false, hidden: true});
+      board.cells.push({row: countRow, col: countCol, isMine: false, isMarked: false, hidden: true});
       countCol = countCol + 1;
     }
     countCol = 0;
@@ -45,7 +52,17 @@ function createBoard(squares){
   }
 }
 
+layMines(3);
 
+function layMines(mineCount){
+  while (mineCount>0){
+    i = Math.floor(Math.random() * 9);
+    if(board.cells[i].isMine !== true){
+      board.cells[i].isMine = true;
+      mineCount = mineCount -1;
+    }
+  }
+}
 
 function startGame () {
   // Don't remove this function call: it makes the game work!
@@ -54,6 +71,7 @@ function startGame () {
     }
   lib.initBoard()
 }
+
 
 // Define this function to look for a win condition:
 //
@@ -69,6 +87,7 @@ function checkForWin () {
     }
   }
   lib.displayMessage('You win!')
+  playSound('sounds/clap.mp3')
 }
 // You can use this function call to declare a winner (once you've
   // detected that they've won, that is!)
